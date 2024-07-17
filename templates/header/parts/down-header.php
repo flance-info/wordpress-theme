53596eb7eb99eac2018712e72256b5180589225f
Rusty <105621220+balconsoftakhmedov@users.noreply.github.com>
2024-07-17 15:49:07 +0500

    added

--- templates/header/parts/down-header.php
+++ templates/header/parts/down-header.php
@@ -2,5 +2,87 @@
 
 ?>
 <div class="main-redcontable-new">
+	<section class="logo-wrapper-wrapper">
+		<div class="logo-wrapper">
+			<div class="logo-and-search">
+				<header class="search-bar-wrapper">
+					<div class="search-input-and-buttons">
+						<div class="search-bar">
+							<?php get_template_part( 'templates/header/parts/logo-header' ); ?>
+							<div class="search-input-wrapper">
+								<div class="search-input">
+									<button class="button">
+										<img
+												class="icon-btn-left"
+												alt=""
+												src="./wp-content/themes/masterstudy-lms-starter-child/assets/images/iconbtnleft.svg"
+										/>
 
+										<div class="text-sm">Categorías</div>
+										<img
+												class="icon-btn-right"
+												alt=""
+												src="./wp-content/themes/masterstudy-lms-starter-child/assets/images/iconbtnright.svg"
+										/>
+									</button>
+									<div class="input-search">
+										<div class="input-element">
+											<img
+													class="icon-addon"
+													loading="lazy"
+													alt=""
+													src="./wp-content/themes/masterstudy-lms-starter-child/assets/images/iconaddon.svg"
+											/>
+
+											<div class="search">Search</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="user-actions">
+							<div class="user-buttons">
+								<div class="button1">
+									<img
+											class="icon"
+											loading="lazy"
+											alt=""
+											src="./wp-content/themes/masterstudy-lms-starter-child/assets/images/icon.svg"
+									/>
+								</div>
+								<div class="button2">
+									<img
+											class="icon-btn-left1"
+											alt=""
+											src="./wp-content/themes/masterstudy-lms-starter-child/assets/images/iconbtnright.svg"
+									/>
+
+									<div class="user-icon-labels">Registrarse</div>
+									<img
+											class="icon-btn-right1"
+											alt=""
+											src="./wp-content/themes/masterstudy-lms-starter-child/assets/images/iconbtnright.svg"
+									/>
+								</div>
+								<button class="button3">
+									<img
+											class="icon-btn-left2"
+											alt=""
+											src="./wp-content/themes/masterstudy-lms-starter-child/assets/images/iconbtnleft-1.svg"
+									/>
+
+									<div class="text-sm1">Iniciar sesión</div>
+									<img
+											class="icon-btn-right2"
+											alt=""
+											src="./wp-content/themes/masterstudy-lms-starter-child/assets/images/iconbtnleft-1.svg"
+									/>
+								</button>
+							</div>
+						</div>
+					</div>
+				</header>
+			</div>
+		</div>
+	</section>
 </div>
