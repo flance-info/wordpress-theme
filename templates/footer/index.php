ce3c7fc71eff04b25468472565009dee3b140e5e
Rusty <105621220+balconsoftakhmedov@users.noreply.github.com>
2024-07-17 20:03:22 +0500

    fixes testimoniy

--- templates/footer/index.php
+++ templates/footer/index.php
@@ -19,7 +19,7 @@ $socials_socials_color_hover = get_theme_mod( 'ms_lms_starter_socials_color_hove
                 <img
                   class="a13193-red-contable-logo-ap-11"
                   alt=""
-                  src="wp-content/themes/masterstudy-lms-starter-child/assets/images//a13193-red-contable-logo-ap-1-1@2x.png"
+                  src="wp-content/themes/masterstudy-lms-starter-child/assets/images/a13193-red-contable-logo-ap-1-1@2x.png"
                 />
                 <div class="copyright-wrapper">
                   <div class="copyright-2025">Copyright @ 2025. All rights reserved.</div>
@@ -29,27 +29,27 @@ $socials_socials_color_hover = get_theme_mod( 'ms_lms_starter_socials_color_hove
                 <div class="button-parent4">
                   <div class="button20">
                   <a href="<?php echo esc_url( $socials_facebook_url ); ?>">
-                    <img class="icon9" alt="" src="wp-content/themes/masterstudy-lms-starter-child/assets/images//icon-9.svg" />
+                    <img class="icon9" alt="" src="wp-content/themes/masterstudy-lms-starter-child/assets/images/icon-9.svg" />
                    </a>
                   </div>
                   <div class="button21">
                   <a href="<?php echo esc_url( $socials_instagram_url ); ?>">
-                    <img class="icon10" alt="" src="wp-content/themes/masterstudy-lms-starter-child/assets/images//icon-10@2x.png" />
+                    <img class="icon10" alt="" src="wp-content/themes/masterstudy-lms-starter-child/assets/images/icon-10@2x.png" />
                     </a>
                   </div>
                   <div class="button22">
                   <a href="#">
-                    <img class="icon11" alt="" src="wp-content/themes/masterstudy-lms-starter-child/assets/images//icon-11.svg" />
+                    <img class="icon11" alt="" src="wp-content/themes/masterstudy-lms-starter-child/assets/images/icon-11.svg" />
                     </a>
                   </div>
                   <div class="button23">
                   <a href="<?php echo esc_url( $socials_twitter_url ); ?>">
-                    <img class="icon12" alt="" src="wp-content/themes/masterstudy-lms-starter-child/assets/images//icon-12.svg" />
+                    <img class="icon12" alt="" src="wp-content/themes/masterstudy-lms-starter-child/assets/images/icon-12.svg" />
                     </a>
                   </div>
                   <div class="button24">
                   <a href="<?php echo esc_url( $socials_youtube_url); ?>">
-                    <img class="icon13" alt="" src="wp-content/themes/masterstudy-lms-starter-child/assets/images//icon-13.svg" />
+                    <img class="icon13" alt="" src="wp-content/themes/masterstudy-lms-starter-child/assets/images/icon-13.svg" />
                     </a>
                   </div>
                 </div>
@@ -59,13 +59,13 @@ $socials_socials_color_hover = get_theme_mod( 'ms_lms_starter_socials_color_hove
           <div class="menu">
             <div class="base-navbar-menu">
               <div class="inner">
-                <img class="home-03-icon" alt="" src="wp-content/themes/masterstudy-lms-starter-child/assets/images//home03.svg" />
+                <img class="home-03-icon" alt="" src="wp-content/themes/masterstudy-lms-starter-child/assets/images/home03.svg" />
 
                 <div class="nav-menu">Home</div>
                 <img
                   class="arrow-down-01-icon1"
                   alt=""
-                  src="wp-content/themes/masterstudy-lms-starter-child/assets/images//arrowdown011.svg"
+                  src="wp-content/themes/masterstudy-lms-starter-child/assets/images/arrowdown011.svg"
                 />
               </div>
             </div>
@@ -74,14 +74,14 @@ $socials_socials_color_hover = get_theme_mod( 'ms_lms_starter_socials_color_hove
                 <img
                   class="icon-placeholder"
                   alt=""
-                  src="wp-content/themes/masterstudy-lms-starter-child/assets/images//iconbtnright.svg"
+                  src="wp-content/themes/masterstudy-lms-starter-child/assets/images/iconbtnright.svg"
                 />
 
                 <div class="nav-menu1">About us</div>
                 <img
                   class="arrow-down-01-icon2"
                   alt=""
-                  src="wp-content/themes/masterstudy-lms-starter-child/assets/images//arrowdown011.svg"
+                  src="wp-content/themes/masterstudy-lms-starter-child/assets/images/arrowdown011.svg"
                 />
               </div>
             </div>
@@ -90,14 +90,14 @@ $socials_socials_color_hover = get_theme_mod( 'ms_lms_starter_socials_color_hove
                 <img
                   class="icon-placeholder1"
                   alt=""
-                  src="wp-content/themes/masterstudy-lms-starter-child/assets/images//iconbtnright.svg"
+                  src="wp-content/themes/masterstudy-lms-starter-child/assets/images/iconbtnright.svg"
                 />
 
                 <div class="nav-menu2">Contacts</div>
                 <img
                   class="arrow-down-01-icon3"
                   alt=""
-                  src="wp-content/themes/masterstudy-lms-starter-child/assets/images//arrowdown011.svg"
+                  src="wp-content/themes/masterstudy-lms-starter-child/assets/images/arrowdown011.svg"
                 />
               </div>
             </div>
@@ -106,14 +106,14 @@ $socials_socials_color_hover = get_theme_mod( 'ms_lms_starter_socials_color_hove
                 <img
                   class="icon-placeholder2"
                   alt=""
-                  src="wp-content/themes/masterstudy-lms-starter-child/assets/images//iconbtnright.svg"
+                  src="wp-content/themes/masterstudy-lms-starter-child/assets/images/iconbtnright.svg"
                 />
 
                 <div class="nav-menu3">Terms & conditions</div>
                 <img
                   class="arrow-down-01-icon4"
                   alt=""
-                  src="wp-content/themes/masterstudy-lms-starter-child/assets/images//arrowdown011.svg"
+                  src="wp-content/themes/masterstudy-lms-starter-child/assets/images/arrowdown011.svg"
                 />
               </div>
             </div>
