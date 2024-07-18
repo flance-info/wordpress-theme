a320dd3f569ae91779381463ca3da4616881ad2b
Rusty <105621220+balconsoftakhmedov@users.noreply.github.com>
2024-07-18 11:31:30 +0500

    categories page

--- partials/vc_parts/courses_categories/style_6.php
+++ partials/vc_parts/courses_categories/style_6.php
@@ -19,143 +19,59 @@ if ( ! empty( $taxonomy ) ) :
 	} else {
 		$terms = explode( ',', str_replace( ' ', '', $taxonomy ) );
 	}
-	if ( ! empty( $terms ) && is_array( $terms ) ) : ?>
-		<div class="stm_lms_courses_categories <?php echo esc_attr( $style . $css_class ); ?>">
+	?>
+	<div class="main-redcontable-new">
+		<section class="logo-wrapper-wrapper">
+			<div class="logo-wrapper">
+				<div class="top-subjects-parent">
+					<h2 class="top-subjects">Top subjects</h2>
+					<div class="button-card-parent">
 
-			<?php
-			foreach ( $terms as $key => $term ) :
-				$term = get_term_by(
-						'id',
-						$term,
-						'stm_lms_course_taxonomy'
-				);
-				if ( empty( $term ) || is_wp_error( $term ) ) {
-					continue;
-				}
-				$term_icon  = get_term_meta( $term->term_id, 'course_icon', true );
-				$term_icon  = ( ! empty( $term_icon ) ) ? $term_icon : 'no-icon';
-				$term_color = get_term_meta( $term->term_id, 'course_color', true );
-				$term_color = ( ! empty( $term_color ) ) ? $term_color : '#1ec1d9';
-				?>
-				<a href="<?php echo esc_url( STM_LMS_Course::courses_page_url() . '?terms[]=' . $term->term_id . '&category[]=' . $term->term_id ); ?>>" title="<?php echo esc_attr( $term->name ); ?>" class="stm_lms_courses_category no_deco"
-				   style="background-color: <?php echo esc_attr( $term_color ); ?>">
-					<i class="<?php echo esc_html( $term_icon ); ?>"></i>
-					<h4><?php echo esc_html( $term->name ); ?></h4>
-				</a>
+						<?php
+						if ( ! empty( $terms ) && is_array( $terms ) ) : ?>
 
-			<?php endforeach; ?>
-		</div>
-	<?php endif; ?>
-<?php endif; ?>
-<div class="main-redcontable-new">
-	<section class="logo-wrapper-wrapper">
-		<div class="logo-wrapper">
-			<div class="top-subjects-parent">
-				<h2 class="top-subjects">Top subjects</h2>
-				<div class="button-card-parent">
-					<div class="button-card">
-						<img
-								class="card-header-icon"
-								loading="lazy"
-								alt=""
-								src="wp-content/themes/masterstudy-lms-starter-child/assets/images/frame-1.svg"
-						/>
 
-						<div class="card-content">
-							<div class="labels">
-								<div class="button-label">Administración de Empresas</div>
-							</div>
-						</div>
-					</div>
-					<div class="button-card1">
-						<img
-								class="button-card-child"
-								alt=""
-								src="wp-content/themes/masterstudy-lms-starter-child/assets/images/frame-1-1.svg"
-						/>
+							<?php
+							foreach ( $terms as $key => $term ) :
+								$term = get_term_by(
+										'id',
+										$term,
+										'stm_lms_course_taxonomy'
+								);
+								if ( empty( $term ) || is_wp_error( $term ) ) {
+									continue;
+								}
+								$term_image       = wpcfto_get_term_meta_text( $term->term_id, 'course_image' );
+							 $media_url = ($term_image) ? wp_get_attachment_url($term_image) : '';
+								?>
+								<div class="button-card">
+								<?php if ($media_url) : ?>
+									<img
+											class="card-header-icon"
+											loading="lazy"
+											alt=""
+											src="<?php echo esc_attr($media_url) ?>"
+									/>
+									<?php endif; ?>
+									<div class="card-content">
+										<a href="<?php echo esc_url( STM_LMS_Course::courses_page_url() . '?terms[]=' . $term->term_id . '&category[]=' . $term->term_id ); ?>>" title="<?php echo esc_attr( $term->name ); ?>" class="">
 
-						<div class="labels-wrapper">
-							<div class="labels1">
-								<div class="button-label1">Construcción de viviendas</div>
-							</div>
-						</div>
-					</div>
-					<div class="button-card2">
-						<img
-								class="button-card-item"
-								alt=""
-								src="wp-content/themes/masterstudy-lms-starter-child/assets/images/frame-1-2.svg"
-						/>
+											<div class="labels">
+												<div class="button-label"><?php echo esc_html( $term->name ); ?></div>
+											</div>
+										</a>
+									</div>
 
-						<div class="labels-container">
-							<div class="labels2">
-								<div class="button-label2">Sales and marketing</div>
-							</div>
-						</div>
-					</div>
-					<div class="button-card3">
-						<img
-								class="button-card-inner"
-								alt=""
-								src="wp-content/themes/masterstudy-lms-starter-child/assets/images/frame-1-3.svg"
-						/>
-
-						<div class="labels-frame">
-							<div class="labels3">
-								<div class="button-label3">Planificación urbana</div>
-							</div>
-						</div>
-					</div>
-					<div class="button-card4">
-						<img class="frame-icon" alt="" src="wp-content/themes/masterstudy-lms-starter-child/assets/images/frame-1-4.svg"/>
-
-						<div class="frame-div">
-							<div class="labels4">
-								<div class="button-label4">World languages</div>
-							</div>
-						</div>
-					</div>
-					<div class="button-card5">
-						<img
-								class="button-card-child1"
-								alt=""
-								src="wp-content/themes/masterstudy-lms-starter-child/assets/images/frame-1-5.svg"
-						/>
-
-						<div class="labels-wrapper1">
-							<div class="labels5">
-								<div class="button-label5">Gestión financiera</div>
-							</div>
-						</div>
-					</div>
-					<div class="button-card6">
-						<img
-								class="button-card-child2"
-								alt=""
-								src="wp-content/themes/masterstudy-lms-starter-child/assets/images/frame-1-6.svg"
-						/>
-
-						<div class="labels-wrapper2">
-							<div class="labels6">
-								<div class="button-label6">Software development</div>
-							</div>
-						</div>
-					</div>
-					<div class="button-card7">
-						<img
-								class="button-card-child3"
-								alt=""
-								src="wp-content/themes/masterstudy-lms-starter-child/assets/images/frame-1-7.svg"
-						/>
-
-						<div class="labels-wrapper3">
-							<div class="labels7">
-								<div class="button-label7">Business planning</div>
-							</div>
-						</div>
+								</div>
+							<?php endforeach; ?>
+						<?php endif; ?>
 					</div>
 				</div>
 			</div>
-		</div>
-	</section>
-</div>
+		</section>
+	</div>
+<?php endif; ?>
+
+
+
+
