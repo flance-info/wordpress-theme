9c4da83434606178f586c27008b3db30030fd224
Rusty <105621220+balconsoftakhmedov@users.noreply.github.com>
2024-07-18 12:51:15 +0500

    courses card

---
+++ stm-lms-templates/elementor-widgets/courses/courses-archive/main.php
@@ -0,0 +1,486 @@
+<div class="main-redcontable-new">
+	<section class="logo-wrapper-wrapper">
+		<div class="logo-wrapper">
+			<div class="popular-courses">
+				<div class="popular-courses-container">
+					<div class="popular-course-heading">
+						<h2 class="cursos-populares">Cursos Populares</h2>
+					</div>
+					<div class="container">
+						<div class="item-course-main">
+							<div class="image-3-parent">
+								<img
+										class="image-3-icon"
+										alt=""
+										src="wp-content/themes/masterstudy-lms-starter-child/assets/images/image-3@2x.png"
+								/>
+								<div class="badge">
+									<div class="new">New</div>
+								</div>
+							</div>
+							<div class="course-title-and-author">
+								<div class="ultimate-2024-web">
+									Ultimate 2024 Web Development Bootcamp
+								</div>
+								<div class="by-alex-johnson">By Alex Johnson</div>
+								<div class="course-details">
+									<div class="hours-wrapper">
+										<div class="hours">40 hours</div>
+									</div>
+									<div class="course-level-icon-wrapper">
+										<div class="course-level-icon"></div>
+									</div>
+									<div class="beginner">Beginner</div>
+								</div>
+								<div class="course-button-container">
+									<button class="button4">
+										<img
+												class="icon-btn-left3"
+												alt=""
+												src="wp-content/themes/masterstudy-lms-starter-child/assets/images/iconbtnright.svg"
+										/>
+
+										<div class="course-action-label">Enroll now</div>
+										<img
+												class="icon-btn-right3"
+												alt=""
+												src="wp-content/themes/masterstudy-lms-starter-child/assets/images/iconbtnright.svg"
+										/>
+									</button>
+									<div class="button5">
+										<img class="icon1" alt="" src="wp-content/themes/masterstudy-lms-starter-child/assets/images/icon.svg"/>
+									</div>
+								</div>
+							</div>
+						</div>
+	<div class="item-course-main">
+							<div class="image-3-parent">
+								<img
+										class="image-3-icon"
+										alt=""
+										src="wp-content/themes/masterstudy-lms-starter-child/assets/images/image-3@2x.png"
+								/>
+								<div class="badge">
+									<div class="new">New</div>
+								</div>
+							</div>
+							<div class="course-title-and-author">
+								<div class="ultimate-2024-web">
+									Ultimate 2024 Web Development Bootcamp
+								</div>
+								<div class="by-alex-johnson">By Alex Johnson</div>
+								<div class="course-details">
+									<div class="hours-wrapper">
+										<div class="hours">40 hours</div>
+									</div>
+									<div class="course-level-icon-wrapper">
+										<div class="course-level-icon"></div>
+									</div>
+									<div class="beginner">Beginner</div>
+								</div>
+								<div class="course-button-container">
+									<button class="button4">
+										<img
+												class="icon-btn-left3"
+												alt=""
+												src="wp-content/themes/masterstudy-lms-starter-child/assets/images/iconbtnright.svg"
+										/>
+
+										<div class="course-action-label">Enroll now</div>
+										<img
+												class="icon-btn-right3"
+												alt=""
+												src="wp-content/themes/masterstudy-lms-starter-child/assets/images/iconbtnright.svg"
+										/>
+									</button>
+									<div class="button5">
+										<img class="icon1" alt="" src="wp-content/themes/masterstudy-lms-starter-child/assets/images/icon.svg"/>
+									</div>
+								</div>
+							</div>
+						</div>
+	<div class="item-course-main">
+							<div class="image-3-parent">
+								<img
+										class="image-3-icon"
+										alt=""
+										src="wp-content/themes/masterstudy-lms-starter-child/assets/images/image-3@2x.png"
+								/>
+								<div class="badge">
+									<div class="new">New</div>
+								</div>
+							</div>
+							<div class="course-title-and-author">
+								<div class="ultimate-2024-web">
+									Ultimate 2024 Web Development Bootcamp
+								</div>
+								<div class="by-alex-johnson">By Alex Johnson</div>
+								<div class="course-details">
+									<div class="hours-wrapper">
+										<div class="hours">40 hours</div>
+									</div>
+									<div class="course-level-icon-wrapper">
+										<div class="course-level-icon"></div>
+									</div>
+									<div class="beginner">Beginner</div>
+								</div>
+								<div class="course-button-container">
+									<button class="button4">
+										<img
+												class="icon-btn-left3"
+												alt=""
+												src="wp-content/themes/masterstudy-lms-starter-child/assets/images/iconbtnright.svg"
+										/>
+
+										<div class="course-action-label">Enroll now</div>
+										<img
+												class="icon-btn-right3"
+												alt=""
+												src="wp-content/themes/masterstudy-lms-starter-child/assets/images/iconbtnright.svg"
+										/>
+									</button>
+									<div class="button5">
+										<img class="icon1" alt="" src="wp-content/themes/masterstudy-lms-starter-child/assets/images/icon.svg"/>
+									</div>
+								</div>
+							</div>
+						</div>
+	<div class="item-course-main">
+							<div class="image-3-parent">
+								<img
+										class="image-3-icon"
+										alt=""
+										src="wp-content/themes/masterstudy-lms-starter-child/assets/images/image-3@2x.png"
+								/>
+								<div class="badge">
+									<div class="new">New</div>
+								</div>
+							</div>
+							<div class="course-title-and-author">
+								<div class="ultimate-2024-web">
+									Ultimate 2024 Web Development Bootcamp
+								</div>
+								<div class="by-alex-johnson">By Alex Johnson</div>
+								<div class="course-details">
+									<div class="hours-wrapper">
+										<div class="hours">40 hours</div>
+									</div>
+									<div class="course-level-icon-wrapper">
+										<div class="course-level-icon"></div>
+									</div>
+									<div class="beginner">Beginner</div>
+								</div>
+								<div class="course-button-container">
+									<button class="button4">
+										<img
+												class="icon-btn-left3"
+												alt=""
+												src="wp-content/themes/masterstudy-lms-starter-child/assets/images/iconbtnright.svg"
+										/>
+
+										<div class="course-action-label">Enroll now</div>
+										<img
+												class="icon-btn-right3"
+												alt=""
+												src="wp-content/themes/masterstudy-lms-starter-child/assets/images/iconbtnright.svg"
+										/>
+									</button>
+									<div class="button5">
+										<img class="icon1" alt="" src="wp-content/themes/masterstudy-lms-starter-child/assets/images/icon.svg"/>
+									</div>
+								</div>
+							</div>
+						</div>
+	<div class="item-course-main">
+							<div class="image-3-parent">
+								<img
+										class="image-3-icon"
+										alt=""
+										src="wp-content/themes/masterstudy-lms-starter-child/assets/images/image-3@2x.png"
+								/>
+								<div class="badge">
+									<div class="new">New</div>
+								</div>
+							</div>
+							<div class="course-title-and-author">
+								<div class="ultimate-2024-web">
+									Ultimate 2024 Web Development Bootcamp
+								</div>
+								<div class="by-alex-johnson">By Alex Johnson</div>
+								<div class="course-details">
+									<div class="hours-wrapper">
+										<div class="hours">40 hours</div>
+									</div>
+									<div class="course-level-icon-wrapper">
+										<div class="course-level-icon"></div>
+									</div>
+									<div class="beginner">Beginner</div>
+								</div>
+								<div class="course-button-container">
+									<button class="button4">
+										<img
+												class="icon-btn-left3"
+												alt=""
+												src="wp-content/themes/masterstudy-lms-starter-child/assets/images/iconbtnright.svg"
+										/>
+
+										<div class="course-action-label">Enroll now</div>
+										<img
+												class="icon-btn-right3"
+												alt=""
+												src="wp-content/themes/masterstudy-lms-starter-child/assets/images/iconbtnright.svg"
+										/>
+									</button>
+									<div class="button5">
+										<img class="icon1" alt="" src="wp-content/themes/masterstudy-lms-starter-child/assets/images/icon.svg"/>
+									</div>
+								</div>
+							</div>
+						</div>
+	<div class="item-course-main">
+							<div class="image-3-parent">
+								<img
+										class="image-3-icon"
+										alt=""
+										src="wp-content/themes/masterstudy-lms-starter-child/assets/images/image-3@2x.png"
+								/>
+								<div class="badge">
+									<div class="new">New</div>
+								</div>
+							</div>
+							<div class="course-title-and-author">
+								<div class="ultimate-2024-web">
+									Ultimate 2024 Web Development Bootcamp
+								</div>
+								<div class="by-alex-johnson">By Alex Johnson</div>
+								<div class="course-details">
+									<div class="hours-wrapper">
+										<div class="hours">40 hours</div>
+									</div>
+									<div class="course-level-icon-wrapper">
+										<div class="course-level-icon"></div>
+									</div>
+									<div class="beginner">Beginner</div>
+								</div>
+								<div class="course-button-container">
+									<button class="button4">
+										<img
+												class="icon-btn-left3"
+												alt=""
+												src="wp-content/themes/masterstudy-lms-starter-child/assets/images/iconbtnright.svg"
+										/>
+
+										<div class="course-action-label">Enroll now</div>
+										<img
+												class="icon-btn-right3"
+												alt=""
+												src="wp-content/themes/masterstudy-lms-starter-child/assets/images/iconbtnright.svg"
+										/>
+									</button>
+									<div class="button5">
+										<img class="icon1" alt="" src="wp-content/themes/masterstudy-lms-starter-child/assets/images/icon.svg"/>
+									</div>
+								</div>
+							</div>
+						</div>
+	<div class="item-course-main">
+							<div class="image-3-parent">
+								<img
+										class="image-3-icon"
+										alt=""
+										src="wp-content/themes/masterstudy-lms-starter-child/assets/images/image-3@2x.png"
+								/>
+								<div class="badge">
+									<div class="new">New</div>
+								</div>
+							</div>
+							<div class="course-title-and-author">
+								<div class="ultimate-2024-web">
+									Ultimate 2024 Web Development Bootcamp
+								</div>
+								<div class="by-alex-johnson">By Alex Johnson</div>
+								<div class="course-details">
+									<div class="hours-wrapper">
+										<div class="hours">40 hours</div>
+									</div>
+									<div class="course-level-icon-wrapper">
+										<div class="course-level-icon"></div>
+									</div>
+									<div class="beginner">Beginner</div>
+								</div>
+								<div class="course-button-container">
+									<button class="button4">
+										<img
+												class="icon-btn-left3"
+												alt=""
+												src="wp-content/themes/masterstudy-lms-starter-child/assets/images/iconbtnright.svg"
+										/>
+
+										<div class="course-action-label">Enroll now</div>
+										<img
+												class="icon-btn-right3"
+												alt=""
+												src="wp-content/themes/masterstudy-lms-starter-child/assets/images/iconbtnright.svg"
+										/>
+									</button>
+									<div class="button5">
+										<img class="icon1" alt="" src="wp-content/themes/masterstudy-lms-starter-child/assets/images/icon.svg"/>
+									</div>
+								</div>
+							</div>
+						</div>
+	<div class="item-course-main">
+							<div class="image-3-parent">
+								<img
+										class="image-3-icon"
+										alt=""
+										src="wp-content/themes/masterstudy-lms-starter-child/assets/images/image-3@2x.png"
+								/>
+								<div class="badge">
+									<div class="new">New</div>
+								</div>
+							</div>
+							<div class="course-title-and-author">
+								<div class="ultimate-2024-web">
+									Ultimate 2024 Web Development Bootcamp
+								</div>
+								<div class="by-alex-johnson">By Alex Johnson</div>
+								<div class="course-details">
+									<div class="hours-wrapper">
+										<div class="hours">40 hours</div>
+									</div>
+									<div class="course-level-icon-wrapper">
+										<div class="course-level-icon"></div>
+									</div>
+									<div class="beginner">Beginner</div>
+								</div>
+								<div class="course-button-container">
+									<button class="button4">
+										<img
+												class="icon-btn-left3"
+												alt=""
+												src="wp-content/themes/masterstudy-lms-starter-child/assets/images/iconbtnright.svg"
+										/>
+
+										<div class="course-action-label">Enroll now</div>
+										<img
+												class="icon-btn-right3"
+												alt=""
+												src="wp-content/themes/masterstudy-lms-starter-child/assets/images/iconbtnright.svg"
+										/>
+									</button>
+									<div class="button5">
+										<img class="icon1" alt="" src="wp-content/themes/masterstudy-lms-starter-child/assets/images/icon.svg"/>
+									</div>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
+
+
+
+<div class="ms_lms_courses_archive">
+	<?php if ( ! empty( $show_header ) ) { ?>
+		<div class="ms_lms_courses_archive__title <?php echo esc_attr( ! empty( $header_presets ) ? $header_presets : '' ); ?>">
+			<h2>
+				<?php echo esc_html( $title_text ); ?>
+			</h2>
+			<?php
+			if ( ! empty( $show_sorting ) && ! empty( $sorting_data['sort_options'] ) ) {
+				STM_LMS_Templates::show_lms_template(
+					"elementor-widgets/courses/courses-archive/sorting/{$sort_presets}",
+					array(
+						'sorting_data' => $sorting_data,
+					)
+				);
+			}
+			?>
+		</div>
+	<?php } ?>
+	<div class="ms_lms_courses_archive__content <?php echo esc_attr( ( ( ! empty( $filter_data['filter_position'] ) ) ? $filter_data['filter_position'] : '' ) . ( ( ! empty( $header_presets ) ) ? " title_{$header_presets}" : '' ) ); ?>">
+		<?php
+		if ( ! empty( $show_filter ) && ! empty( $filter_data['filter_options'] ) ) {
+			STM_LMS_Templates::show_lms_template(
+				'elementor-widgets/courses/courses-archive/filter/main',
+				array(
+					'show_filter' => $show_filter,
+					'filter_data' => $filter_data,
+				)
+			);
+		}
+		?>
+		<div class="ms_lms_courses_card_wrapper">
+			<?php if ( ! empty( $featured_courses ) && is_array( $featured_courses ) && ! empty( $show_featured_block ) ) { ?>
+				<div class="ms_lms_courses_card featured <?php echo ( ! empty( $course_card_presets ) ) ? esc_attr( $course_card_presets ) : ''; ?>">
+					<?php
+					STM_LMS_Templates::show_lms_template(
+						"elementor-widgets/courses/card/{$course_card_presets}/main",
+						array(
+							'courses'             => $featured_courses,
+							'course_image_size'   => $course_image_size,
+							'meta_slots'          => $meta_slots,
+							'card_data'           => $card_data,
+							'popup_data'          => $popup_data,
+							'course_card_presets' => $course_card_presets,
+							'widget_type'         => $widget_type,
+						)
+					);
+					?>
+				</div>
+			<?php } ?>
+			<div class="ms_lms_courses_card <?php echo ( ! empty( $course_card_presets ) ) ? esc_attr( $course_card_presets ) : ''; ?>">
+				<?php
+				if ( ! empty( $featured_courses ) && is_array( $featured_courses ) && empty( $show_featured_block ) ) {
+					STM_LMS_Templates::show_lms_template(
+						"elementor-widgets/courses/card/{$course_card_presets}/main",
+						array(
+							'courses'             => $featured_courses,
+							'course_image_size'   => $course_image_size,
+							'meta_slots'          => $meta_slots,
+							'card_data'           => $card_data,
+							'popup_data'          => $popup_data,
+							'course_card_presets' => $course_card_presets,
+							'featured'            => 'featured',
+							'widget_type'         => $widget_type,
+						)
+					);
+				}
+				if ( ! empty( $courses ) && is_array( $courses ) ) {
+					STM_LMS_Templates::show_lms_template(
+						"elementor-widgets/courses/card/{$course_card_presets}/main",
+						array(
+							'courses'             => $courses,
+							'course_image_size'   => $course_image_size,
+							'meta_slots'          => $meta_slots,
+							'card_data'           => $card_data,
+							'popup_data'          => $popup_data,
+							'course_card_presets' => $course_card_presets,
+							'widget_type'         => $widget_type,
+						)
+					);
+				} else {
+					STM_LMS_Templates::show_lms_template( 'elementor-widgets/courses/courses-archive/filter/no-results' );
+				}
+				?>
+			</div>
+			<div class="ms_lms_courses_archive__pagination_wrapper">
+				<?php
+				if ( ! empty( $show_pagination ) && $pagination_data['total_pages'] > 1 ) {
+					STM_LMS_Templates::show_lms_template(
+						"elementor-widgets/courses/courses-archive/pagination/{$pagination_presets}",
+						array(
+							'pagination_data' => $pagination_data,
+						)
+					);
+				}
+				?>
+			</div>
+		</div>
+	</div>
+</div>
